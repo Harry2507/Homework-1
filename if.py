e2a458dddd34c109a6950c1651913a04e7139245
Harry2507 <49687673+Harry2507@users.noreply.github.com>
2019-04-23 19:31:10 +0700

    first post

---
+++ if.py
@@ -0,0 +1,8 @@
+x = int(input('Enter a choice: '))
+if x == 3:
+    print("x equals 3.")
+elif x == 2:
+    print("x equals 2.")
+else:
+    print("x equals something slse.")
+print("This is outside the 'if'")
